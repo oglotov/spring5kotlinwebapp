9c38f6636374fa1004aa9239f33ae625ebe93fa0
Oleksandr Glotov <alexander.v.glotov@gmail.com>
2022-11-21 23:14:34 +0200

    Initial commit

---
+++ src/test/kotlin/ua/wwind/spring5kotlinwebapp/Spring5kotlinwebappApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.wwind.spring5kotlinwebapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring5kotlinwebappApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
